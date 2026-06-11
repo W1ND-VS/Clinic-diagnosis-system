645393033f66256a09f73b359ac1cdef91165959
W1ND-VS <wind090303@gmail.com>
2026-06-12 00:49:56 +0700

    remove env

--- backend_for_CMS/app/config.py
+++ backend_for_CMS/app/config.py
@@ -6,9 +6,7 @@ load_dotenv()  # Tải biến môi trường từ file .env nếu có
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "mysql+pymysql://root:@localhost:3307/db_clinic"
-    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "f7lL5JcQoNZaEOUrMKWfRMkgFtMBMpOJHtwp4Ap08iQ")
     JWT_SECRET_KEY = os.getenv(
