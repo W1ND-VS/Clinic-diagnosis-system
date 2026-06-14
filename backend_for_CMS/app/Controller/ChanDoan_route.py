8b51c25e11400cb461d9790d800b4f0981664e00
W1ND-VS <wind090303@gmail.com>
2026-06-14 23:34:56 +0700

    Update Relative path for assets

--- backend_for_CMS/app/Controller/ChanDoan_route.py
+++ backend_for_CMS/app/Controller/ChanDoan_route.py
@@ -362,10 +362,14 @@ def predict_disease():
             return error_response(
                 "Thiếu hoặc sai định dạng trường 'symptoms' (phải là list)", 400
             )
+        # Đường dẫn đến thư mục chứa file assits
+        assits_folder = "../assits"
+        if assits_folder is None:
+            print("Warning: 'assits_folder' is not set. Please provide the correct path to the assits folder.")
 
         # Gọi hàm dự đoán (pipeline mới, dùng file trong assits)
         results = chandoan_service.diagnose_disease(
-            symptoms, top_k=5, assits_folder="d:/CTU/Nien luan nganh/Clinic_Management_System/backend_for_CMS/app/assits"
+            symptoms, top_k=5, assits_folder=assits_folder
         )
         # Lấy thông tin chi tiết bệnh cho từng disease_code
         detailed_results = []
